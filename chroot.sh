afbc24b49c47121fbb8f7e65c671508df48093e8
Slemen <Slemen@ukr.net>
2021-11-23 11:57:58 +0200

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo ' Скрипт второй настройки системы в chroot'
 
-#'Часовой пояс'
 ln -sf /usr/share/zoneinfo/Europe/Kiev /etc/localtime
 hwclock --systohc
 echo " Часовой пояс установлен"
 
-#'Локализация'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
@@ -21,7 +19,6 @@ echo " Используйте в имени только буквы латинс
 echo ""
 read -p " Введите имя пользователя: " username
 
-#'Настройка сети'
 echo $hostname > /etc/hostname
 echo "127.0.0.1   localhost" >> /etc/hosts
 echo "::1         localhost" >> /etc/hosts
@@ -29,7 +26,9 @@ echo "127.0.1.1	  $hostname.localdomain $hostname" >> /etc/hosts
 nano /etc/hosts
 clear
 
-#'Пароль суперпользователя'
+mkinitcpio -p linux
+clear
+
 echo " Укажите пароль для ROOT"
 passwd
 echo ""
@@ -39,7 +38,6 @@ echo ' Добавляем пароль для пользователя '$usernam
 echo ""
 passwd $username
 
-echo " Получение привилегий для ROOT"
 nano /etc/sudoers
 clear
 pacman -Syy --noconfirm
@@ -48,10 +46,6 @@ echo " Установка UEFI-GRUB"
 pacman -S grub --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
-
-#'Создание нового образа Initramfs'
-mkinitcpio -p linux
-clear
 
 echo ""
 echo " Настроим multilib ?"
@@ -73,7 +67,7 @@ clear
 echo " Multilib репозиторий настроен"
 fi
 
-pacman -Sy xorg-server xf86-video-intel --noconfirm
+pacman -Sy xorg xorg-server xf86-video-intel --noconfirm
 clear
 
 echo " Добавление хука автоматической очистки кэша pacman"
