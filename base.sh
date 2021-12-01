5e5ea215c9a648b0eab35719249fff04edb23fba
Slemen <Slemen@ukr.net>
2021-12-01 16:28:17 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -50,66 +50,15 @@ mount /dev/sda1 /mnt/boot/efi
 clear
 lsblk
 
-pacman -Sy --noconfirm 
+echo " Установка основных пакетов"
+pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano wpa_supplicant dialog btrfs-progs intel-ucode
+
+echo " Настройка системы"
+genfstab -U /mnt >> /mnt/etc/fstab
 clear
 
-echo ""
-echo " Если у вас есть wifi модуль и вы сейчас его не используете, то для"
-echo " исключения ошибок в работе системы рекомендую "1" "
-echo ""
-echo ' Установка основных пакетов, будете ли вы использовать wifi ?'
-while
-    read -n1 -p  "
- 1 - да
- 2 - нет: " x_pacstrap  # sends right after the keypress
-    echo ''
-    [[ "$x_pacstrap" =~ [^12] ]]
-do
-    :
-done
- if [[ $x_pacstrap == 1 ]]; then
-  clear
-  pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano wpa_supplicant dialog btrfs-progs intel-ucode
-  genfstab -pU /mnt >> /mnt/etc/fstab
-elif [[ $x_pacstrap == 2 ]]; then
-  clear
-  pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano btrfs-progs intel-ucode
-  genfstab -pU /mnt >> /mnt/etc/fstab
-fi
- clear
-
-echo " Если вы производите установку используя Wifi тогда рекомендую  "1" "
-echo " Если проводной интернет тогда "2" "
-echo ""
-echo ' wifi или dhcpcd ?'
-while
-    read -n1 -p  "
- 1 - wifi 
- 2 - dhcpcd: " int # sends right after the keypress
-    echo ''
-    [[ "$int" =~ [^12] ]]
-do
-    :
-done
-if [[ $int == 1 ]]; then
-
-curl -LO https://raw.githubusercontent.com/Slemen/Archlinux/master/chroot.sh
-  mv chroot.sh /mnt
-  chmod +x /mnt/chroot.sh
-
-  echo " Первый этап готов"
-  echo " Arch-chroot"
-  echo ' 1. проверь  интернет для продолжения установки в черуте || 2.команда для запуска ./chroot.sh '
-  arch-chroot /mnt
+echo " Первый скрипт установки готов"
+arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Slemen/Archlinux/master/chroot.sh)"
 umount -a
 reboot
-  elif [[ $int == 2 ]]; then
-  echo " Первый этап готов"
-  echo " Arch-chroot"
-  arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Slemen/Archlinux/master/chroot.sh)"
-  umount -a
-reboot
-fi
-elif [[ $menu == 0 ]]; then
 exit
-fi
