524dc13bdb5955e1ee29ba5f3d004dc1ee9f91de
Slemen <Slemen@ukr.net>
2021-12-17 12:59:10 +0200

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -84,27 +84,25 @@ echo "Установка Plasma KDE и дополнительных програ
 
 pacman -Sy plasma kde-system-meta kio-extras konsole yakuake htop dkms --noconfirm
 
-pacman -S alsa-utils ark aspell aspell-en aspell-ru audacious audacious-plugins bat bind --noconfirm
+pacman -S arch-install-scripts alsa-utils ark aspell aspell-en aspell-ru hspell libvoikko hunspell-ru audacious bat bind rsync duf --noconfirm
 
-pacman -S firefox firefox-i18n-ru dnsmasq dolphin-plugins fd filelight fzf git meld --noconfirm
+pacman -S dolphin-plugins fd filelight meld firefox firefox-i18n-ru fzf gvfs-mtp --noconfirm
 
-pacman -S kcalc fish telegram-desktop gvfs gvfs-mtp gwenview haveged --noconfirm
+pacman -S tig git kcalc gwenview haveged highlight kfind lib32-alsa-plugins kdeconnect sshfs --noconfirm
 
-pacman -S highlight kfind lib32-alsa-plugins lib32-freetype2 lib32-glu lib32-libcurl-gnutls --noconfirm
+pacman -S lib32-freetype2 lib32-glu lib32-libcurl-gnutls lib32-libpulse lib32-libxft lib32-libxinerama --noconfirm
 
-pacman -S lib32-libpulse lib32-libxft lib32-libxinerama lib32-libxrandr lib32-openal lib32-openssl-1.0 --noconfirm
+pacman -S lib32-libxrandr lib32-openal lib32-openssl-1.0 lib32-sdl2_mixer nano-syntax-highlighting --noconfirm
 
-pacman -S lib32-sdl2_mixer nano-syntax-highlighting neofetch noto-fonts-emoji okular perl-image-exiftool --noconfirm
+pacman -S noto-fonts-emoji p7zip pcmanfm perl-image-exiftool xdg-desktop-portal --noconfirm
 
-pacman -S pcmanfm p7zip pulseaudio-alsa bash-language-server --noconfirm
+pacman -S plasma5-applets-weather-widget python-pip python-virtualenv python-lsp-server bash-language-server qbittorrent --noconfirm
 
-pacman -S qbittorrent plasma5-applets-weather-widget qt5-xmlpatterns --noconfirm
+pacman -S kate smplayer smplayer-themes sox spectacle starship telegram-desktop gitui --noconfirm
 
-pacman -S kate smplayer smplayer-themes spectacle terminus-font kdeconnect sshfs --noconfirm
+pacman -S terminus-font ttf-arphic-ukai ttf-arphic-uming ttf-caladea ttf-carlito ttf-croscore --noconfirm
 
-pacman -S ttf-arphic-ukai ttf-arphic-uming ttf-caladea ttf-carlito ttf-croscore ttf-dejavu --noconfirm
-
-pacman -S ttf-liberation ttf-sazanami unrar xclip zim yt-dlp starship --noconfirm
+pacman -S ttf-dejavu ttf-liberation ttf-sazanami unrar xclip xorg-xrandr yt-dlp zim expac --noconfirm
 clear
 
 #echo ""
@@ -120,10 +118,10 @@ clear
 #pacman -S pamac-aur downgrade yay timeshift ventoy-bin --noconfirm
 #clear
 
-pacman -S libva-utils libva-intel-driver vulkan-intel lib32-libva lib32-libva-intel-driver lib32-vulkan-intel libvdpau-va-gl --noconfirm
+pacman -S libva-utils libva-intel-driver vulkan-intel lib32-libva lib32-libva-intel-driver lib32-vulkan-intel libvdpau-va-gl
 #clear
 
-pacman -S bluez-utils pulseaudio-bluetooth --noconfirm
+pacman -S bluez-utils pulseaudio-bluetooth
 systemctl enable bluetooth.service
 #clear
 
@@ -131,34 +129,34 @@ grub-mkfont -s 16 -o /boot/grub/ter-u16b.pf2 /usr/share/fonts/misc/ter-u16b.otb
 grub-mkconfig -o /boot/grub/grub.cfg
 #clear
 
-pacman -Rns discover plasma-thunderbolt bolt plasma-firewall --noconfirm
+pacman -Rns discover plasma-thunderbolt bolt plasma-firewall
 
-pacman -S xorg-xinit --noconfirm
+pacman -S xorg-xinit
 cp /etc/X11/xinit/xinitrc /home/$username/.xinitrc
 chown $username:users /home/$username/.xinitrc
 chmod +x /home/$username/.xinitrc
 echo "exec startplasma-x11 " >> /home/$username/.xinitrc
 echo ' [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx ' >> /etc/profile
 echo ""
-pacman -R konqueror --noconfirm
+pacman -R konqueror
 #clear
 echo "Plasma KDE и дополнительные программы успешно установлены"
 echo "Установка sddm"
-pacman -S sddm sddm-kcm --noconfirm
+pacman -S sddm sddm-kcm
 systemctl enable sddm.service -f
 echo "[General]" >> /etc/sddm.conf
 echo "Numlock=on" >> /etc/sddm.conf
 #clear
 echo "Установка sddm  завершена "
 
-pacman -Sy networkmanager networkmanager-openvpn network-manager-applet usb_modeswitch --noconfirm
+pacman -Sy networkmanager networkmanager-openvpn network-manager-applet usb_modeswitch
 systemctl enable NetworkManager.service
 systemctl enable ModemManager.service
 #clear
 echo ""
 echo "Установка  программ закончена"
 
-pacman -S tlp tlp-rdw --noconfirm
+pacman -S tlp tlp-rdw
 systemctl enable tlp.service
 systemctl enable NetworkManager-dispatcher.service
 systemctl mask systemd-rfkill.service
