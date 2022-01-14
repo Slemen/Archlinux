bc091847b1f88a3ab73f5db5c4a9c81619821b98
Slemen <Slemen@ukr.net>
2022-01-14 22:00:02 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -64,11 +64,11 @@ done
  if [[ $x_pacstrap == 1 ]]; then
   clear
   pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano wpa_supplicant dialog btrfs-progs intel-ucode iucode-tool
-  genfstab -U /mnt >> /mnt/etc/fstab
+  genfstab -pU /mnt >> /mnt/etc/fstab
 elif [[ $x_pacstrap == 2 ]]; then
   clear
   pacstrap /mnt base dhcpcd linux linux-headers which netctl inetutils pacman-contrib base-devel wget linux-firmware nano btrfs-progs intel-ucode iucode-tool
-  genfstab -U /mnt >> /mnt/etc/fstab
+  genfstab -pU /mnt >> /mnt/etc/fstab
 fi
  clear
 
