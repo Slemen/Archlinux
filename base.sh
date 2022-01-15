6b3ced52b27233e97eb8a9f9bdbb55e1ad8d310a
Slemen <Slemen@ukr.net>
2022-01-15 15:30:05 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-timedatectl set-ntp true
-
 loadkeys ru
 setfont cyr-sun16
 clear
 
-echo''
+timedatectl set-ntp true
+
+echo ""
 echo "Начнём установку? "
 
 while
     read -n1 -p  "
-    1 - да
+ 1 - да
 
-    0 - нет: " hello # sends right after the keypress
+ 0 - нет: " hello # sends right after the keypress
     echo ''
     [[ "$hello" =~ [^10] ]]
 do
@@ -23,206 +23,43 @@ done
   clear
   echo "Добро пожаловать в установку ArchLinux"
   elif [[ $hello == 0 ]]; then
-  exit
+   exit
 fi
-###
-clear
-pacman -Sy --noconfirm
 
-efibootmgr
-echo "Добро пожаловать в установку ArchLinux режим UEFI "
-lsblk -f
-echo " Здесь вы можете удалить boot от старой системы, файлы Windows загрузчика не затрагиваются."
-echo " если вам необходимо полность очистить boot раздел, то пропустите этот этап далее установка предложит отформатировать boot раздел "
-echo " При установке дуал бут раздел не нужно форматировать!!! "
-echo ""
-echo 'удалим старый загрузчик linux'
-while
-    read -n1 -p  "
-    1 - удалим старый загрузчкик линукс
-    0 -(пропустить) - данный этап можно пропустить если установка производиться первый раз или несколько OS  " boots
-    echo ''
-    [[ "$boots" =~ [^10] ]]
-do
-    :
-done
-if [[ $boots == 1 ]]; then
-  clear
- lsblk -f
-  echo ""
-read -p "Укажите boot раздел (sda2/sdb2 ( например sda2 )):" bootd
-mount /dev/$bootd /mnt
-cd /mnt
-ls | grep -v EFI | xargs rm -rfv
-cd /mnt/boot/efi
-ls | grep -v Boot | grep -v Microsoft | xargs rm -rfv
-cd /root
+pacman -Syy --noconfirm
+clear
+
+#gdisk /dev/sda
+cfdisk /dev/sda --zero
+clear
+
+mkfs.vfat -F32 /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mkfs.btrfs -f -L Root /dev/sda3
+
+mount /dev/sda3 /mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
 umount /mnt
-  elif [[ $boots == 0 ]]; then
-   echo " очистка boot раздела пропущена, далее вы сможете его отформатировать! "
-fi
 
-echo " Выбирайте "1", если ранее не производилась разметка диска и у вас нет разделов для ArchLinux "
-echo ""
-echo 'Нужна разметка диска?'
-while
-   read -n1 -p  "
-   1 - да
-   0 - нет: " cfdisk # sends right after the keypress
-    echo ''
-    [[ "$cfdisk" =~ [^10] ]]
-do
-   :
-done
- if [[ $cfdisk == 1 ]]; then
-  clear
- lsblk -f
-  echo ""
-read -p " Укажите диск (sda/sdb/sdc) " cfd
-cfdisk /dev/$cfd
-elif [[ $cfdisk == 0 ]]; then
-echo 'разметка пропущена.'
-fi
-#
- clear
- lsblk -f
-  echo ""
-read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
-echo ""
-mkfs.btrfs -f /dev/$root -L Root
-mount /dev/$root /mnt
-
-btrfs sub cr /mnt/@
-#btrfs subvolume create /mnt/@home
-
-umount /dev/$root
-mount -o rw,noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/$root /mnt
-mkdir -p /mnt/home
-echo ""
-##
- clear
- lsblk -f
-  echo ""
-echo 'форматируем BOOT?'
-while
-    read -n1 -p  "
-    1 - да
-    0 - нет: " boots # sends right after the keypress
-    echo ''
-    [[ "$boots" =~ [^10] ]]
-do
-    :
-done
- if [[ $boots == 1 ]]; then
-  read -p "Укажите BOOT раздел(sda/sdb 1.2.3.4 (sda7 например)):" bootd
-  mkfs.vfat -F32 /dev/$bootd
-  mkdir /mnt/boot/efi
-  mount /dev/$bootd /mnt/boot/efi
-  elif [[ $boots == 0 ]]; then
- read -p "Укажите BOOT раздел(sda/sdb 1.2.3.4 (sda7 например)):" bootd
- mkdir /mnt/boot/efi
-mount /dev/$bootd /mnt/boot/efi
-fi
+mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/sda3 /mnt
+mkdir -p /mnt/{boot/efi,home}
+mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/sda3 /mnt/home
+mount /dev/sda1 /mnt/boot/efi
 clear
-echo ""
-echo " Можно использовать раздел от предыдущей системы( и его не форматировать )
-далее в процессе установки можно будет удалить все скрытые файлы и папки в каталоге
-пользователя"
-echo ""
-echo 'Добавим раздел  HOME ?'
-while
-    read -n1 -p  "
-    1 - да
-    0 - нет: " homes # sends right after the keypress
-    echo ''
-    [[ "$homes" =~ [^10] ]]
-do
-    :
-done
-   if [[ $homes == 0 ]]; then
-     echo 'пропущено'
-  elif [[ $homes == 1 ]]; then
-    echo ' Форматируем HOME раздел?'
-while
-    read -n1 -p  "
-    1 - да
-    0 - нет: " homeF # sends right after the keypress
-    echo ''
-    [[ "$homeF" =~ [^10] ]]
-do
-    :
-done
-   if [[ $homeF == 1 ]]; then
-   echo ""
-   lsblk -f
-#   read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
-#   mkfs.ext4 /dev/$home -L home
-#   mkdir /mnt/home
-#   mount /dev/$home /mnt/home
-    read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
-    mkfs.btrfs -f /dev/$home -L Home
-    mount /dev/$home /mnt/home
-    btrfs sub cr /mnt/@home
-    umount /dev/$home
-    mount -o rw,noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$home /mnt/home
-   elif [[ $homeF == 0 ]]; then
- lsblk -f
-
-# read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
-# mount /dev/$homeV /mnt
-# btrfs sub cr /mnt/@home
-# umount /dev/$homeV
- lsblk -f
-
-# read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
-# mount -o rw,noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/$root /mnt
-# mkdir -p /mnt/home
-# mount -o noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$root /mnt/home
-
- read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
- mount -o rw,noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
-
-# mount /dev/$homeV /mnt/home
-fi
-fi
-############ swap   ####################################################
- clear
- lsblk -f
-  echo ""
-echo 'добавим swap раздел?'
-while
-    read -n1 -p  "
-    1 - да
-
-    0 - нет: " swap # sends right after the keypress
-    echo ''
-    [[ "$swap" =~ [^10] ]]
-do
-    :
-done
- if [[ $swap == 1 ]]; then
-  read -p "Укажите swap раздел(sda/sdb 1.2.3.4 (sda7 например)):" swaps
-  mkswap /dev/$swaps -L swap
-  swapon /dev/$swaps
-  elif [[ $swap == 0 ]]; then
-   echo 'добавление swap раздела пропущено.'
-fi
-##################################################################################
-clear
+lsblk
 
 pacman -Sy --noconfirm
-######
 clear
-echo ""
-echo " Если у вас есть wifi модуль и вы сейчас его не используете, но будете использовать потом то для "
-echo " исключения ошибок в работе системы рекомендую "1" "
+
 echo ""
 echo 'Установка базовой системы, будете ли вы использовать wifi?'
 while
     read -n1 -p  "
-    1 - да
+ 1 - да
 
-    2 - нет: " x_pacstrap  # sends right after the keypress
+ 2 - нет: " x_pacstrap  # sends right after the keypress
     echo ''
     [[ "$x_pacstrap" =~ [^12] ]]
 do
@@ -230,22 +67,22 @@ do
 done
  if [[ $x_pacstrap == 1 ]]; then
   clear
- pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano wpa_supplicant dialog btrfs-progs intel-ucode iucode-tool
- genfstab -U /mnt >> /mnt/etc/fstab
- elif [[ $x_pacstrap == 2 ]]; then
+  pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware dhcpcd netctl inetutils wget pacman-contrib nano wpa_supplicant dialog btrfs-progs intel-ucode iucode-tool
+  genfstab -U /mnt >> /mnt/etc/fstab
+elif [[ $x_pacstrap == 2 ]]; then
   clear
   pacstrap /mnt base dhcpcd linux linux-headers which netctl inetutils pacman-contrib base-devel wget linux-firmware nano btrfs-progs intel-ucode iucode-tool
   genfstab -U /mnt >> /mnt/etc/fstab
-  fi
-##################################################
-clear
-echo "Если вы производите установку используя Wifi тогда рекомендую  "1" "
+fi
+ clear
+
+echo "Если вы производите установку используя Wifi тогда "1" "
 echo ""
 echo "если проводной интернет тогда "2" "
 echo ""
 echo 'wifi или dhcpcd ?'
 while
-    read -n1 -p  "1 - wifi, 2 - dhcpcd: " int # sends right after the keypress
+    read -n1 -p "1 - wifi, 2 - dhcpcd: " int # sends right after the keypress
     echo ''
     [[ "$int" =~ [^12] ]]
 do
@@ -258,7 +95,7 @@ if [[ $int == 1 ]]; then
   chmod +x /mnt/chroot.sh
   echo ""
   echo 'первый этап готов '
-  echo 'archLinux chroot'
+  echo 'ArchLinux chroot'
   echo '1. проверь  интернет для продолжения установки в черуте || 2.команда для запуска ./chroot.sh '
   arch-chroot /mnt
 umount -a
@@ -266,7 +103,7 @@ reboot
   elif [[ $int == 2 ]]; then
   echo ""
   echo 'первый этап готов '
-  echo 'archLinux chroot'
+  echo 'ArchLinux chroot'
   arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Slemen/Archlinux/master/chroot.sh)"
 umount -a
 reboot
