c8151b34d32c7b741ec55fffbb0ea9996b47860b
Slemen <Slemen@ukr.net>
2022-01-16 17:06:22 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -33,48 +33,55 @@ lsblk -f
 echo ""
 echo 'Нужна разметка диска?'
 while
-    read -n1 -p  "
- 1 - да
-
- 0 - нет: " cfdisk # sends right after the keypress
+   read -n1 -p  "
+   1 - да
+   0 - нет: " cfdisk # sends right after the keypress
     echo ''
     [[ "$cfdisk" =~ [^10] ]]
 do
-    :
+   :
 done
  if [[ $cfdisk == 1 ]]; then
-   clear
+  clear
  lsblk -f
   echo ""
-  read -p "Укажите диск (sda/sdb например sda или sdb) : " cfd
+read -p " Укажите диск (sda/sdb/sdc) " cfd
 cfdisk /dev/$cfd
-echo ""
-clear
 elif [[ $cfdisk == 0 ]]; then
-   echo ""
-   clear
-   echo 'разметка пропущена.'
+echo 'разметка пропущена.'
 fi
 #
-  clear
-  lsblk -f
+ clear
+ lsblk -f
   echo ""
-  read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
+read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
 echo ""
 mkfs.btrfs -f /dev/$root -L Root
 mount /dev/$root /mnt
+
 btrfs sub cr /mnt/@
+#btrfs subvolume create /mnt/@home
+
 umount /dev/$root
 mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/$root /mnt
 mkdir -p /mnt/home
-################  home  ############################################################
 echo ""
-echo 'Добавим раздел HOME?'
+##
+ clear
+ lsblk -f
+  echo ""
+
+clear
+echo ""
+echo " Можно использовать раздел от предыдущей системы( и его не форматировать )
+далее в процессе установки можно будет удалить все скрытые файлы и папки в каталоге
+пользователя"
+echo ""
+echo 'Добавим раздел  HOME ?'
 while
     read -n1 -p  "
- 1 - да
-
- 0 - нет: " homes # sends right after the keypress
+    1 - да
+    0 - нет: " homes # sends right after the keypress
     echo ''
     [[ "$homes" =~ [^10] ]]
 do
@@ -83,13 +90,11 @@ done
    if [[ $homes == 0 ]]; then
      echo 'пропущено'
   elif [[ $homes == 1 ]]; then
-  echo ""
-echo 'Форматируем HOME раздел?'
+    echo ' Форматируем HOME раздел?'
 while
     read -n1 -p  "
- 1 - да
-
- 0 - нет: " homeF # sends right after the keypress
+    1 - да
+    0 - нет: " homeF # sends right after the keypress
     echo ''
     [[ "$homeF" =~ [^10] ]]
 do
@@ -98,26 +103,34 @@ done
    if [[ $homeF == 1 ]]; then
    echo ""
    lsblk -f
-   read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
-   mkfs.btrfs -f /dev/$home -L Home
-   mount /dev/$home /mnt
-   btrfs sub cr /mnt/@home
-   umount /dev/$home
-   lsblk -f
-
-   read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
-   mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
-
+#   read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
+#   mkfs.ext4 /dev/$home -L home
+#   mkdir /mnt/home
+#   mount /dev/$home /mnt/home
+    read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
+    mkfs.btrfs -f /dev/$home -L Home
+    mount /dev/$home /mnt/home
+    btrfs sub cr /mnt/home/@home
+    umount /dev/$home
+    mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$home /mnt/home
    elif [[ $homeF == 0 ]]; then
  lsblk -f
- read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
- mkdir /mnt/home
- mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
+
+# read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
+# mount /dev/$homeV /mnt
+# btrfs sub cr /mnt/@home
+# umount /dev/$homeV
  lsblk -f
 
- read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
- mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/$root /mnt
- 
+# read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
+# mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@ /dev/$root /mnt
+# mkdir -p /mnt/home
+# mount -o noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$root /mnt/home
+
+ read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
+ mount -o rw,noatime,compress=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
+
+# mount /dev/$homeV /mnt/home
 fi
 fi
 ########## boot ########
