dd799a0ef047aae40efba8c2ba54f3aa5c6f3da4
Slemen <Slemen@ukr.net>
2022-01-17 17:47:43 +0200

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -32,6 +32,7 @@ fi
 echo "Настройка localtime "
 echo ""
 ln -sf /usr/share/zoneinfo/Europe/Kiev /etc/localtime
+timedatectl set-ntp true
 hwclock --systohc
 echo "Часовой пояс установлен "
 #####################################
