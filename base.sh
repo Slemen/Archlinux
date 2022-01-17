c7b1c67bb07a1c7449c62b4cab370d70b2350357
Slemen <Slemen@ukr.net>
2022-01-17 12:42:27 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -156,6 +156,7 @@ done
  lsblk -f
 
  read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
+mkdir -p /mnt/home
  mount -o rw,noatime,compress-force=zstd,discard=async,autodefrag,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
 
 fi
