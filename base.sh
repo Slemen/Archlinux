1d2151c0d3b259c30d297658c2978dff8d841cab
Slemen <Slemen@ukr.net>
2022-02-01 15:01:17 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -8,6 +8,7 @@ timedatectl set-ntp true
 
 echo ""
 echo "Начнём установку? "
+
 while
     read -n1 -p  "
  1 - да
@@ -22,153 +23,32 @@ done
   clear
   echo "Добро пожаловать в установку ArchLinux"
   elif [[ $hello == 0 ]]; then
-  exit
+   exit
 fi
-###
-pacman -Syy --noconfirm
+
+cfdisk /dev/sda --zero
 clear
-lsblk -f
-##############################
-echo ""
-echo 'Нужна разметка диска?'
-while
-   read -n1 -p  "
- 1 - да
-   
- 0 - нет: " cfdisk # sends right after the keypress
-    echo ''
-    [[ "$cfdisk" =~ [^10] ]]
-do
-   :
-done
- if [[ $cfdisk == 1 ]]; then
-  clear
- lsblk -f
-  echo ""
-read -p " Укажите диск (sda/sdb/sdc) " cfd
-cfdisk /dev/$cfd
-elif [[ $cfdisk == 0 ]]; then
-echo 'Разметка диска пропущена'
-fi
-#
- clear
- lsblk -f
-  echo ""
-read -p "Укажите ROOT раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
-echo ""
-mkfs.btrfs -f /dev/$root -L Root
-mount /dev/$root /mnt
+
+mkfs.vfat -F32 /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mkfs.btrfs -f -L Root /dev/sda3
+
+mount /dev/sda3 /mnt
 btrfs su cr /mnt/@
-umount /dev/$root
-mount -o rw,noatime,compress=zstd,discard=async,space_cache=v2,subvol=@ /dev/$root /mnt
-#mkdir -p /mnt/home
-echo ""
-################ boot ################
- clear
- lsblk -f
-  echo ""
-echo 'Форматируем BOOT?'
-while
-    read -n1 -p  "
- 1 - да
-    
- 0 - нет: " boots # sends right after the keypress
-    echo ''
-    [[ "$boots" =~ [^10] ]]
-do
-    :
-done
- if [[ $boots == 1 ]]; then
- echo ""
-  read -p "Укажите BOOT раздел(sda/sdb 1.2.3.4 (sda7 например)):" bootd
-  mkfs.vfat -F32 /dev/$bootd
-  mkdir -p /mnt/boot/efi
-  mount /dev/$bootd /mnt/boot/efi
-  elif [[ $boots == 0 ]]; then
-  echo ""
-  read -p "Укажите BOOT раздел(sda/sdb 1.2.3.4 (sda7 например)):" bootd
-  mkdir -p /mnt/boot/efi
-  mount /dev/$bootd /mnt/boot/efi
-fi
-################ swap ################
- clear
- lsblk -f
-echo ""
-echo 'Добавим swap раздел?'
-while
-    read -n1 -p  "
- 1 - да
- 
- 0 - нет: " swap # sends right after the keypress
-    echo ''
-    [[ "$swap" =~ [^10] ]]
-do
-    :
-done
- if [[ $swap == 1 ]]; then
- echo ""
-  read -p "Укажите swap раздел(sda/sdb 1.2.3.4 (sda7 например)):" swaps
-  mkswap /dev/$swaps -L swap
-  swapon /dev/$swaps
-  elif [[ $swap == 0 ]]; then
-   echo 'Добавление swap раздела пропущено.'
-fi
-################ home ################
+btrfs su cr /mnt/@home
+umount /mnt
+
+mount -o rw,noatime,compress=zstd,discard=async,space_cache=v2,subvol=@ /dev/sda3 /mnt
+mkdir -p /mnt/{boot/efi,home}
+mount -o rw,noatime,compress=zstd,discard=async,space_cache=v2,subvol=@home /dev/sda3 /mnt/home
+mount /dev/sda1 /mnt/boot/efi
 clear
-echo "Можно использовать раздел от предыдущей системы( и его не форматировать )
-далее в процессе установки можно будет удалить все скрытые файлы и папки в каталоге
-пользователя"
-echo ""
-echo 'Добавим раздел HOME?'
-while
-    read -n1 -p  "
- 1 - да
-    
- 0 - нет: " homes # sends right after the keypress
-    echo ''
-    [[ "$homes" =~ [^10] ]]
-do
-    :
-done
-   if [[ $homes == 0 ]]; then
-     echo 'пропущено'
-  elif [[ $homes == 1 ]]; then
-  echo ""
-  echo 'Форматируем HOME раздел?'
-while
-    read -n1 -p  "
- 1 - да
-    
- 0 - нет: " homeF # sends right after the keypress
-    echo ''
-    [[ "$homeF" =~ [^10] ]]
-do
-    :
-done
-   if [[ $homeF == 1 ]]; then
-   echo ""
-   lsblk -f
-   echo ""
-    read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
-    mkfs.btrfs -f /dev/$home -L Home
-    mkdir -p /mnt/home
-    mount /dev/$home /mnt/home
-    btrfs su cr /mnt/home/@home
-    umount /dev/$home
-    mount -o rw,noatime,compress=zstd,discard=async,space_cache=v2,subvol=@home /dev/$home /mnt/home
-   elif [[ $homeF == 0 ]]; then
- lsblk -f
-echo ""
- read -p "Укажите HOME раздел(sda/sdb 1.2.3.4 (sda6 например)):" homeV
-mkdir -p /mnt/home
- mount -o rw,noatime,compress=zstd,discard=async,space_cache=v2,subvol=@home /dev/$homeV /mnt/home
-fi
-fi
-################################
-clear
+lsblk
+
 pacman -Sy --noconfirm
 clear
-################
+
 echo ""
 echo 'Установка базовой системы, будете ли вы использовать wifi?'
 while
@@ -191,8 +71,7 @@ elif [[ $x_pacstrap == 2 ]]; then
   genfstab -U /mnt >> /mnt/etc/fstab
 fi
  clear
-###############################
-echo ""
+
 echo "Если вы производите установку используя Wifi тогда "1" "
 echo ""
 echo "если проводной интернет тогда "2" "
