30bb43dba34fb77330c9ab31a70675039dec4713
Slemen <Slemen@ukr.net>
2022-02-07 14:53:07 +0200

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -107,7 +107,7 @@ pacman -S lib32-freetype2 lib32-glu lib32-libcurl-gnutls lib32-libpulse lib32-li
 
 pacman -S lib32-libxrandr lib32-openal lib32-openssl-1.0 lib32-sdl2_mixer nano-syntax-highlighting --noconfirm
 
-pacman -S noto-fonts-emoji p7zip pcmanfm pkgfile xdg-desktop-portal-kde bash-language-server --noconfirm
+pacman -S noto-fonts-emoji p7zip pcmanfm pkgfile xdg-desktop-portal xdg-desktop-portal-kde bash-language-server
 
 pacman -S plasma5-applets-weather-widget python-pip python-virtualenv python-lsp-server  qbittorrent --noconfirm
 
